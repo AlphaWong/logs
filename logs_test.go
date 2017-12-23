12a3c584d595999d3c21a4dbb3b56740380cbe1f
alpha.wong <alpha.wong@lalamove.com>
2017-12-23 14:00:25 +0800

    UPT: Readme.

--- logs_test.go
+++ logs_test.go
@@ -1,20 +1,21 @@
-package logs
+package logs_test
 
 import (
 	"testing"
 
+	lalamove "github.com/logs"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetLalamoveLoggerPassDebug(t *testing.T) {
-	Logger().Debug("I am not a Debug")
-	Logger().Info("I am not an Info")
-	Logger().Warn("I am not a Warn")
-	Logger().Error("I am not an Error")
+	lalamove.Logger().Debug("I am not a Debug")
+	lalamove.Logger().Info("I am not an Info")
+	lalamove.Logger().Warn("I am not a Warn")
+	lalamove.Logger().Error("I am not an Error")
 	// It should not be called as the it will return exit code 3
 	// Logger().Fatal("I am not a Fatal")
 	// By default, loggers are unbuffered. However, since zap's low-level APIs allow buffering,
 	// calling Sync before letting your process exit is a good habit.
-	defer Logger().Sync()
+	defer lalamove.Logger().Sync()
 	assert.True(t, true)
 }
